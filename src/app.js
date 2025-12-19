4bebc389fc175f144d5dd5b6507cc4341e042bf7
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-12-19 13:51:49 +0100

    init project scaffolding with empty files and folders

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+  app.js – Express-applikation
+
+  - Definierar middleware och routes
+  - Kopplar routes till controllers
+  - Tar emot HTTP-förfrågningar
+
+  Gör inte: beslutar om regler, bokningar eller SQL
+*/
