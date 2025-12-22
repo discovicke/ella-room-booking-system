2c35d04ed601930d50767d00520a1c6d1231902a
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-12-22 13:16:37 +0100

    change back docs for booking repository

--- src/repositories/booking.repo.js
+++ src/repositories/booking.repo.js
@@ -1,14 +1,16 @@
 /**
- * 📚 USER REPOSITORY
+ * 📚 BOOKING REPOSITORY
  * * PURPOSE:
- * Handles access to the "users" table.
+ * Handles access to the "bookings" table.
  * * SCOPE:
- * - Function: findUserByEmail(email) -> SELECT * FROM users WHERE email = ?
- * - Used during login to verify if a user exists.
+ * - Function: createBooking(data) -> INSERT INTO bookings...
+ * - Function: findBookingsByRoom(roomId) -> SELECT ...
+ * - Function: findBookingsByUser(userId) -> SELECT ...
  * * RELATION:
  * - Imports: 'src/db/query.js'
- * - Imported by: 'src/controllers/auth.controller.js'
+ * - Imported by: 'src/controllers/bookings.controller.js'
  */
+
 import { db } from "../db/db.js";
 
 const query = db.prepare("SELECT * FROM bookings");
