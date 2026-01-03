c0dbd0b0bfeb0f84fc58c555c0636f8ba6815431
Marcus Löf <LeafMaster1@outlook.com>
2026-01-03 13:21:58 +0100

    changed fix for delete and update user

--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser, getAllUsers, getUserById } from "./user.controller.js";
+import { createUser, getAllUsers, getUserById, deleteUser, updateUser } from "./user.controller.js";
 import { authenticate } from "../../middleware/authentication.middleware.js";
 import { authorize } from "../../middleware/authorization.middleware.js";
 import { ROLES } from "../../constants/roles.js";
+
 
 const userRouter = express.Router();
 
@@ -14,5 +15,11 @@ userRouter.get("/:id", authenticate, authorize(ROLES.ADMIN), getUserById);
 
 // POST /api/users - Create a new user (Admin only)
 userRouter.post("/", authenticate, authorize(ROLES.ADMIN), createUser);
+
+// PUT /api/users/:id - Update a user (Admin only)
+userRouter.put('/:id', authenticate, authorize(ROLES.ADMIN), updateUser);
+
+// DELETE /api/users/:id - Delete a user (Admin only)
+userRouter.delete("/:id", authenticate, authorize(ROLES.ADMIN), deleteUser);
 
 export default userRouter;
